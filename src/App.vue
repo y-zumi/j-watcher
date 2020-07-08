dbc65e2d0341f6de79f3e57513c8611c30f1325d
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-07-09 01:38:53 +0900

    add(api): add api call for youtube

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <a v-bind:href="live_link">live link</a>
+    {{youtube_live}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      live_link: "",
+      youtube_live: ""
+    };
+  },
+  created() {
+    fetch(
+      // "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&channelId=UCj8BadK_leFelzdbEZnKRZg&key="
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&channelId=UCJubINhCcFXlsBwnHp0wl_g&key="
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        this.live_link =
+          "https://www.youtube.com/watch?v=" + json.items[0].id.videoId;
+        this.youtube_live = JSON.stringify(
+          json.items[0].snippet.liveBroadcastContent
+        );
+      })
+      .catch(err => {
+        this.youtube_live = err;
+      });
   }
-}
+};
 </script>
 
 <style>
