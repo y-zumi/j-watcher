e7f79996cc01a4a6461ed15f7e3b6dc1640f542d
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-07-12 01:18:11 +0900

    add(api): add twitch api

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <a v-bind:href="youtube_live_link">youtube live link</a>
-    {{youtube_live}}
+
+    <a v-bind:href="twitch_live_link">twitch live link</a>
+    {{twitch_live}}
     <br />
     <a v-bind:href="niconico_live_link">niconico live link</a>
     {{niconico_live}}
+    <br />
+    <a v-bind:href="youtube_live_link">youtube live link</a>
+    {{youtube_live}}
+    <br />
   </div>
 </template>
 
@@ -20,18 +25,58 @@ export default {
     HelloWorld
   },
   data() {
+    if (process.env.VUE_APP_ENV == "production") {
+      return {
+        youtube_live_link:
+          "https://www.youtube.com/channel/UCx1nAvtVDIsaGmCMSe8ofsQ",
+        youtube_live: "none",
+        youtube_channel_id: "UCx1nAvtVDIsaGmCMSe8ofsQ",
+        niconico_live_link: "https://live.nicovideo.jp/",
+        niconico_live: "none",
+        niconico_channel_id: "",
+        twitch_live_link: "https://www.twitch.tv/kato_junichi0817",
+        twitch_live: "none",
+        twitch_user_id: "545050196"
+      };
+    }
     return {
-      youtube_live_link: "",
-      youtube_live: "",
-      niconico_live_link: "",
-      niconico_live: "",
-      youtube_channel_id: "UCj8BadK_leFelzdbEZnKRZg"
+      youtube_live_link:
+        "https://www.youtube.com/channel/UCx1nAvtVDIsaGmCMSe8ofsQ",
+      youtube_live: "none",
+      youtube_channel_id: "UCj8BadK_leFelzdbEZnKRZg",
+      niconico_live_link: "https://live.nicovideo.jp/",
+      niconico_live: "none",
+      niconico_channel_id: "",
+      twitch_live_link: "https://www.twitch.tv/kato_junichi0817",
+      twitch_live: "none",
+      twitch_user_id: "545050196"
     };
   },
   created() {
     axios
       .get(
-        "/niconico_api/api/v2/live/contents/search?q=LiVE&_sort=-userId&targets=title&fields=title"
+        "https://api.twitch.tv/helix/streams?user_id=" + this.twitch_user_id,
+        {
+          headers: {
+            Authorization: "Bearer " + process.env.VUE_APP_TWITCH_AUTH_TOKEN,
+            "Client-ID": process.env.VUE_APP_TWITCH_CLIENT_ID
+          }
+        }
+      )
+      .then(response => {
+        console.log(response.data);
+        return response.data;
+      })
+      .then(json => {
+        this.twitch_live = json.data[0].type;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    axios
+      .get(
+        "https://api.search.nicovideo.jp/api/v2/live/contents/search?q=LiVE&_sort=-userId&targets=title&fields=title"
       )
       .then(response => {
         console.log(response.data);
@@ -42,12 +87,12 @@ export default {
           "https://live2.nicovideo.jp/watch/" + json.data[0].contentId;
       })
       .catch(err => {
-        this.niconico_live = err;
+        console.log(err);
       });
 
     axios
       .get(
-        "/youtube_api/youtube/v3/search?part=snippet&eventType=live&type=video&channelId=" +
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&channelId=" +
           this.youtube_channel_id +
           "&key=" +
           process.env.VUE_APP_YOUTUBE_API_KEY
@@ -58,11 +103,12 @@ export default {
         return response.data;
       })
       .then(json => {
+        this.youtube_live = json.items[0].snippet.liveBroadcastContent;
         this.youtube_live_link =
           "https://www.youtube.com/watch?v=" + json.items[0].id.videoId;
       })
       .catch(err => {
-        this.youtube_live = err;
+        console.log(err);
       });
   }
 };
