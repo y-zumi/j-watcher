3a738e20d196c8e2c64c7d85f2645eda4dccb17e
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-07-12 01:17:28 +0900

    fix(config): add secure parameter

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,13 @@ module.exports = {
       "^/niconico_api": {
         target: "https://api.search.nicovideo.jp",
         changeOrigin: true,
+        secure: false,
         pathRewrite: { "^/niconico_api": "" }
       },
       "^/youtube_api": {
         target: "https://www.googleapis.com",
         changeOrigin: true,
+        secure: false,
         pathRewrite: { "^/youtube_api": "" }
       }
     }
