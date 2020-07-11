c4583be15bb981ce7acd64fa04cabed4879f86ac
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-07-11 18:27:41 +0900

    fix(config): fix proxy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   devServer: {
     proxy: {
-      "/niconico_api/": {
-        target: "https://api.search.nicovideo.jp/api",
+      "^/niconico_api": {
+        target: "https://api.search.nicovideo.jp",
+        changeOrigin: true,
+        pathRewrite: { "^/niconico_api": "" }
+      },
+      "^/youtube_api": {
+        target: "https://www.googleapis.com",
+        changeOrigin: true,
+        pathRewrite: { "^/youtube_api": "" }
       }
     }
   }
